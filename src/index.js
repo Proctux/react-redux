05fcd250a0ec3a77488a013619c0e33277607237
Vinicius de Morais Justo <vjusto91@gmail.com>
2018-02-10 14:22:38 -0200

    feat(redux): Integrate redux with main application

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 
 import App from '_views/app'
 
 import './bootstrap'
+import setupApp from './setupApp'
 import registerServiceWorker from './registerServiceWorker'
 
-render(<App />, document.getElementById('root'))
-registerServiceWorker()
+const root = () =>
+  setupApp().then(({ store }) => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById('root')
+    )
+    registerServiceWorker()
+  })
+
+root()
