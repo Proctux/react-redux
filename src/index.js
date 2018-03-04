e543c27712f200653d0d0db43a45b69f92d32bc9
Vinicius de Morais Justo <vjusto91@gmail.com>
2018-03-04 20:52:39 -0300

    refactor(store): Change store configuration and add dev middlewares to reducer

--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import { renderRoutes } from 'react-router-config'
+import createHistory from 'history/createBrowserHistory'
 
 import './bootstrap'
-import setupApp from './setupApp'
+import configureStore from './store/configure-store'
 import routes from './routes'
 
-const root = () =>
-  setupApp().then(({ store, history }) => {
-    render(
-      <Provider store={store}>
-        <ConnectedRouter history={history}>{renderRoutes(routes)}</ConnectedRouter>
-      </Provider>,
-      document.getElementById('root')
-    )
-  })
+const root = () => {
+  const history = createHistory()
+  // eslint-disable-next-line no-underscore-dangle
+  const store = configureStore(history, window.__INITIAL_STATE__ || {})
+  render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>{renderRoutes(routes)}</ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+  )
+}
 
 root()
--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -2,16 +2,23 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import promise from 'redux-promise-middleware'
 import { routerMiddleware } from 'react-router-redux'
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension' // eslint-disable-line import/no-extraneous-dependencies
 
 import rootReducer from '_modules/reducers'
 
 const DEFAULT_STATE = {}
 
-const configureStore = (history, preloadedState = DEFAULT_STATE) =>
-  createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(thunk, promise(), routerMiddleware(history))
-  )
+const configureStore = (history, preloadedState = DEFAULT_STATE) => {
+  const middlewares = [thunk, promise(), routerMiddleware(history)]
+  if (process.env.NODE_ENV === 'development') {
+    return createStore(
+      rootReducer,
+      preloadedState,
+      composeWithDevTools(applyMiddleware(...middlewares, logger))
+    )
+  }
+  return createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
+}
 
 export default configureStore
