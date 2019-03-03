437194b9002ce2c9a829f7da7a7a5e5eaa8cebe0
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-03-02 22:00:54 -0300

    Remove global variables
    It is not a good practice to do so

--- tests/setup.js
+++ tests/setup.js
@@ -1,10 +1,5 @@
-import { shallow, render, mount } from 'enzyme'
 import '@babel/polyfill'
 import 'raf-polyfill'
-
-global.shallow = shallow
-global.render = render
-global.mount = mount
 
 // Fail tests on any console error
 console.error = message => {
