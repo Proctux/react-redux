6ea60bee16b2f351e399f8a2f1b06d939bc6d2f1
leonardogobbilopez <leonardoglopez@gmail.com>
2020-08-23 20:14:53 -0300

    fix: Remove car reducer change

--- src/modules/car/reducer.js
+++ src/modules/car/reducer.js
@@ -6,7 +6,7 @@ import { GET_PERFECT_BMW } from './actions'
 const INITIAL_STATE = new Car()
 
 const car = createReducer(INITIAL_STATE, {
-  [GET_PERFECT_BMW.FULFILLED]: (state, { payload }) => new Car(payload),
+  [GET_PERFECT_BMW.FULFILLED]: (state, { payload }) => state.mergeProps(new Car(payload)),
 })
 
 export default car
