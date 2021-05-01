a79d60380645c05acf222b7f53f089e5a1e00e65
Proctux <luiz.ado@hotmail.com>
2021-05-01 13:52:49 -0300

    build: Initial setup

--- src/views/app/index.jsx
+++ src/views/app/index.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-// <i18n>
-import { useTranslation } from 'react-i18next'
-// </i18n>
 
 import BMW from '_assets/images/bmw328i.jpg'
 import { getPerfectBMW } from '_modules/car/actions'
@@ -10,9 +7,6 @@ import { getPerfectBMW } from '_modules/car/actions'
 import styles from './styles.css'
 
 const App = () => {
-  // <i18n>
-  const { t } = useTranslation()
-  // </i18n>
   const car = useSelector(state => state.car)
   const dispatch = useDispatch()
 
@@ -26,21 +20,6 @@ const App = () => {
         <img src={BMW} alt="BMW" className={styles['App-logo']} />
         <h1 className={styles['App-title']}>Jungle Devs Boilerplate</h1>
       </header>
-      {/* <i18n> */}
-      <p className={styles['App-intro']}>
-        {car.marca
-          ? t('carEvaluation', {
-              brand: car.marca,
-              fipe: car.codigoFipe,
-              fuel: car.combustivel,
-              model: car.modelo,
-              value: car.valor,
-              year: car.anoModelo,
-            })
-          : t('loading')}
-      </p>
-      {/* </i18n> */}
-      {/* <no-i18n> */}
       {car.marca ? (
         <p
           className={styles['App-intro']}
@@ -48,7 +27,6 @@ const App = () => {
       ) : (
         <p className={styles['App-intro']}>Loading...</p>
       )}
-      {/* </no-i18n> */}
     </div>
   )
 }
